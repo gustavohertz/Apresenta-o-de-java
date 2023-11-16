6ca188edf583e311fd7e4278ef82c964a8f8126a
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-11-16 07:25:26 -0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Customer {
+    private String name = null;
+    private String postcode = null;
+    private int telephone = 0;
+    private int customerId = 0;
+
+    private static int customerCount = 001;
+
+    public Customer(String cName, String pcode, int tel){
+            name = cName;
+            postcode = pcode;
+            telephone = tel;
+            customerId = customerCount++;
+    }
+    public int getCustomerId(){
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public int getTelephone() { return telephone;}
+}
